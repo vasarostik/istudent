3b11e4238b9f8535359299c1982d77c178966e2a
Rostyk Hlynka <hlynka1608@gmail.com>
2019-04-20 20:34:31 +0300

    mainPageUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Header } from "./components/Header";
 
 class App extends Component {
+  
   render() {
+
+    const companyButton = {
+      height:'8vmin',
+      width:'22vmin',
+      border: '0px',
+      fontSize:'3.5vmin',
+      color: '#fff',
+      boxShadow: 'rgba(0, 0, 0, 0.2) -0.5vmin 0.7vmin 0.6vmin 0px',
+      backgroundColor: '#404040'
+    };
+
+    const studentButton = {
+      height:'8vmin',
+      width:'22vmin',
+      border: '0px',
+      fontSize:'3.5vmin',
+      color: '#fff',
+      boxShadow: 'rgba(0, 0, 0, 0.2) -0.5vmin 0.7vmin 0.6vmin 0px',
+      backgroundColor: '#78cc1f'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        {/* main container */}
+        <div className="main">
+          <Header />
+          <div className="topic">
+
+            <h2 className="leftTitle">
+              For both IT Company and
+            </h2>
+
+            <div className="shape">
+              <div className="rectangle">
+                <div className="emptyRectangle">
+                  <h2 className="rightTitle"> Students</h2>
+                </div>
+              </div>
+            </div>
+
+            <p className="pTitle">
+              Find a job of your dream<br />
+              in any Ukrainian's IT Company<br />
+              or find intelligent students on your<br />
+              successful project!
+            </p>
+          </div>
+          <div className="buttons">
+            <button style={companyButton}>Company</button>
+            <button style={studentButton}>Student</button>
+          </div>
+        </div>
+        {/* end of main container */}
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../css/Header.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+export class Header extends React.Component {
+
+    render() {
+
+        return (
+            <Navbar className="mainNav" collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
