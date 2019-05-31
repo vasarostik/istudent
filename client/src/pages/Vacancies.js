5ca602cb039a2ff11ee837327c4d9d6acbed772c
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-01 01:03:19 +0300

    vacancies(add backend)

--- client/src/pages/Vacancies.js
+++ client/src/pages/Vacancies.js
@@ -2,8 +2,57 @@ import React, { Component } from 'react';
 import '../css/Vacancies.css';
 import { CardDeck, Card } from 'react-bootstrap';
 import { AuthNavbar } from '../components/AuthNavbar';
+import { getJwt } from '../helpers/jwt';
 
 export class Vacancies extends Component {
+    constructor() {
+        super();
+        this.state = {
+            vacancies: [],
+        };
+
+    }
+
+    componentDidMount() {
+
+        let token  = getJwt();
+        fetch('/vacancy', { 
+            method: 'get', 
+            headers: new Headers({
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json',
+                Authorization: `Bearer ${token}`
+            })
+          }).then(results => {
+
+                console.log('dsd' + results);
+                return results.json();
+            }).then(data => {
+                let vacancies = data.map((vac) => {
+                    return (
+                        <Card style={{ flex: '0 0 500px', marginTop: '50px' }}>
+                            <Card.Body>
+                                <div style={{ margin: '0 10px', fontWeight: '600', fontSize: '16px' }}>
+                                    <p style={{ float: 'left', color: '#78cc1f' }}>position</p>
+                                    <p style={{ float: 'right', color: '#78cc1f' }}>Company<span style={{ paddingLeft: '65px', marginRight: '42px' }}>$</span></p>
+                                </div>
+                                <div style={{ marginLeft: '10px', marginTop: '5vh', paddingBottom: '5vh' }}>
+                                    <Card.Title style={{ float: 'left' }}>{vac.Position.name}</Card.Title>
+                                    <Card.Title style={{ float: 'right', textTransform: 'uppercase' }}>{vac.Company.name}<span style={{ paddingLeft: '76px', marginRight: '10px  ' }}>{vac.salaryFrom} - {vac.salaryTo}</span></Card.Title>
+                                </div>
+                                <p style={{ margin: '0 10px', fontWeight: '600', fontSize: '16px', float: 'left', color: '#78cc1f' }}>required skills</p>
+                                <ul className="reqs" style={{ marginTop: '5vh', marginLeft: '-25px' }}>
+                                    <li><span>{vac.detail}</span></li>
+                                </ul>
+                            </Card.Body>
+                            <button className="applyBtn">Apply</button>
+                        </Card>
+                    )
+                })
+                this.setState({ vacancies: vacancies });
+                console.log("state", this.state.vacancies);
+            })
+    }
 
     render() {
 
@@ -13,23 +62,7 @@ export class Vacancies extends Component {
                 {/* main container */}
                 <div className="vacancies">
                     <CardDeck style={{ width: '100%', display: 'flex', flexWrap: 'wrap', alignItems: 'stretch' }}>
-                        <Card style={{ flex: '0 0 500px', marginTop: '50px' }}>
-                            <Card.Body>
-                                <div style={{ margin: '0 10px', fontWeight: '600', fontSize: '16px' }}>
-                                    <p style={{ float: 'left', color: '#78cc1f' }}>position</p>
-                                    <p style={{ float: 'right', color: '#78cc1f' }}>location <span style={{ paddingLeft: '50px', marginRight: '20px' }}>$</span></p>
-                                </div>
-                                <div style={{ marginLeft: '10px', marginTop: '5vh', paddingBottom: '5vh' }}>
-                                    <Card.Title style={{ float: 'left' }}>C++ Developer</Card.Title>
-                                    <Card.Title style={{ float: 'right', textTransform: 'uppercase' }}>Lviv <span style={{ paddingLeft: '70px', marginRight: '10px  ' }}>800</span></Card.Title>
-                                </div>
-                                <p style={{ margin: '0 10px', fontWeight: '600', fontSize: '16px', float: 'left', color: '#78cc1f' }}>required skills</p>
-                                <ul className="reqs" style={{  marginTop: '5vh', marginLeft: '-25px' }}>
-                                    <li><span>blablabalblabla ablba ablab ablaa</span></li>
-                                </ul>
-                            </Card.Body>
-                            <button className="applyBtn">Apply</button>
-                        </Card>
+                        {this.state.vacancies}
                     </CardDeck>
                 </div>
                 {/* end of main container */}
--- routes/vacancy.js
+++ routes/vacancy.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const vacancy = require("../controllers/vacancy");
-/*
 router.get("/", vacancy.findAll);
-router.post("/", vacancy.findVacancyByData);
-router.put("/",vacancy.updateVacancy);
-router.delete("/", vacancy.deleteVacancy());
-*/
+// router.post("/", vacancy.findVacancyByData);
+// router.put("/",vacancy.updateVacancy);
+// router.delete("/", vacancy.deleteVacancy());
+
 module.exports = router;
